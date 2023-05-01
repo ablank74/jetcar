58b66a9be534e4014c6b9421c2f6c0f14ccabffc
Adam Blank <adam@12thfloor.com>
2023-05-01 16:42:25 -0500

    Fixed bugs

--- test/launch.sh
+++ test/launch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Check if the current user is in the Docker group
-if [[ $(id -nG) =~ "docker" ]]; then
+if [[ $(id -nG) =~ "docker" ]] ; then
+    echo "$USER in Docker Group"
 else
   # Add the current user to the Docker group
   echo "Adding $USER to the Docker group"
@@ -10,12 +11,12 @@ else
   sudo systemctl restart docker
 fi
 # Check if the "test_car" image exists
-if sudo docker images -q test_car >/dev/null 2>&1 ; then
+if docker images -q test_car >/dev/null 2>&1 ; then
     echo "The 'test_car' image exists, launching with lidar at /dev/ttyUSB0 and camera at /dev/video0"
 else
     echo "The 'test_car' image does not exist, creating it"
-    ~/jetcar/test/start.sh
+    ~/jetcar/test/build.sh
 fi
 
-docker run -it --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 --device=/dev/video0:/dev/video0 -p 8888:8888 -v /home/adam/jetcar/test/notebooks:/root/notebooks test_car
+docker run -it --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 --device=/dev/video0:/dev/video0 -p 8888:8888 -p 5000:5000 -v /home/adam/jetcar/test/notebooks:/root/notebooks test_car
 #
