5c8c5d1b6c9d0f0fe2962bedb07929aac6431cf2
Adam Blank <adam@12thfloor.com>
2023-05-01 09:12:15 -0500

    Working Build!!

--- test/launch.sh
+++ test/launch.sh
@@ -1,2 +1,2 @@
-docker run -it --rm --device=/dev/video0:/dev/video0 -p 8888:8888 -v /home/jetson/jetcar/test/notebooks:/root/notebooks test_car
-#--device=/dev/ttyUSB0:/dev/ttyUSB0
+docker run -it --rm --device=/dev/ttyUSB0:/dev/ttyUSB0 --device=/dev/video0:/dev/video0 -p 8888:8888 -v /home/jetson/jetcar/test/notebooks:/root/notebooks test_car
+#
